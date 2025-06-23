aafab66fb4658a45465a4fbc87bba95d2eefabc5
elhalj <wilsonikedakoffi7@gmail.com>
2025-06-23 18:18:30 +0000

    refactor

--- src/middleware/protectUserRoute.js
+++ src/middleware/protectUserRoute.js
@@ -2,20 +2,29 @@ import jwt from "jsonwebtoken";
 import { User } from "../models/user.model.js";
 
 export const protectRoute = async (req, res, next) => {
-  try {
-    const token = req.cookies.jwt;
-    if (!token) {
-      return res
-        .status(401)
-        .json({ message: "Unauthorized - No token provided" });
-    }
+  let token;
 
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    token = req.headers.authorization.split(" ")[1];
+  } else if (
+    typeof req.cookies.token !== "undefined" &&
+    req.cookies.token !== null
+  ) {
+    token = req.cookies.token;
+  }
+
+  if (!token) {
+    return res
+      .status(401)
+      .json({ message: "Unauthorized - No token provided" });
+  }
+  try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
-    const user = await User.findById(decoded.userId).select("-password");
-    if (!user) {
-      return res.status(404).json({ message: "User not found" });
-    }
+    const user = await User.findById(decoded.user.id).select("-password");
 
     req.user = user;
     next();
